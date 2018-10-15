126a32095cbfd68b04d582d28a163ca038347873
npte <kamazoh@inbox.ru>
2018-10-15 10:11:22 +0300

    initial commit

---
+++ src/main/java/ru/npte/sloth/App.java
@@ -0,0 +1,13 @@
+package ru.npte.sloth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
